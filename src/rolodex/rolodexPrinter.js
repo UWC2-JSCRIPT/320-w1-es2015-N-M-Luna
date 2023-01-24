a6af92514fc8f1aa604657200f92be63efaa6718
N. M. Luna <nat.luna.64@gmail.com>
2023-01-23 17:11:33 -0800

    try to fix failing check

--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,4 +1,5 @@
 import people from './people.json' assert { type: 'json' };
+// import * as people from './people.json'; did not work
 
 people.forEach((person) => {
   const { name, email, phone } = person;
